c07f67ec67e637acf0ab991d540b1370bb8ca776
galenwinsor <galen_winsor@brown.edu>
2024-03-19 22:29:26 -0400

    feat: add some fake stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { faker } from '@faker-js/faker';
+
+	const randomName = () => {
+		const randomAnimal = faker.animal.type();
+		const randomPreposition = faker.word.preposition();
+		const randomObject = faker.word.noun();
+
+		return `${randomAnimal} ${randomPreposition} the ${randomObject}`;
+	};
+
+	let name = '';
+</script>
+
+<div class="column-layout">
+	<main class="pt-12">
+		<h1>Old Time Name Generator</h1>
+		<p>{name}</p>
+		<button on:click={() => (name = randomName())}>Randomize</button>
+	</main>
+</div>
+
+<style>
+	.column-layout {
+		display: grid;
+		grid-template-columns: 1fr min(900px, 100%) 1fr;
+	}
+
+	.column-layout > * {
+		grid-column: 2;
+	}
+</style>
